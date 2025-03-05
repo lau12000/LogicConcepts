5e7f09842a61a3bce2bc9a0f66005a35641268a8
laura <laurapalacios1206@gmail.com>
2025-03-04 20:48:09 -0500

    Completeexercice01

---
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,29 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.WriteLine("Ingrese número o 'salir' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt} es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El valor ingresado {numberString} no es un número");
+    }
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over");
